762d0094acf0f14c26cd12df5831c37551a2c2a6
KenM <kem406@.harvard.edu>
2021-02-06 20:52:56 -0500

    Meeting changes

--- main/obd/slice_test.py
+++ main/obd/slice_test.py
@@ -4,7 +4,7 @@ import main.obd.obd as ob
 import imageio
 import matplotlib.pyplot as plt
 
-filename = 'main/obd/f.hdf5'
+filename = 'main/obd/f1.hdf5'
 with h5py.File(filename, "r") as f:
     # List all groups
     print("Keys: %s" % f.keys())
@@ -13,20 +13,21 @@ with h5py.File(filename, "r") as f:
     # Get the data
     data = list(f[a_group_key])
 
-h5py.File.close()
-
 A=np.array(data)
-fig, ax = plt.subplots(1,2, figsize=(24., 8.))
-ax[0].imshow(A, origin='lower')
+fig, ax = plt.subplots(1,1, figsize=(24., 8.))
+ax.imshow(np.abs(A), origin='lower')
 plt.show()
 
-tables.file._open_files.close_all()
-
-filename2 = 'main/obd/f2.hdf5'
-with h5py.File(filename2, "r") as f:
+filename2 = 'main/obd/f2.h5'
+with h5py.File(filename2, "r") as f2:
     # List all groups
-    print("Keys: %s" % f.keys())
-    a_group_key = list(f.keys())[0]
+    print("Keys: %s" % f2.keys())
+    a_group_key = list(f2.keys())[0]
 
     # Get the data
-    data = list(f[a_group_key])
+    data2 = list(f2[a_group_key])
+
+B=np.array(data2)
+fig, ax = plt.subplots(1,1, figsize=(24., 8.))
+ax.imshow(B, origin='lower')
+plt.show()
